25db6f14a7858fe6507480a07bc841ccde1c000e
DineshSagar <59312687+Dineshsagar@users.noreply.github.com>
2025-09-09 23:26:49 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,5 @@
 ### Pre-Release Version V1.0###
-# Not a complete code - Includes only partial features out of actual intended application #
-
+# Not a complete code - Includes only partial features out of actual intended application 
 
 import soundcard as sc
 import soundfile as sf
